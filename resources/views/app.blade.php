198299045f17268866c22db414fc14f39f09069c
serg1999g <46674079+serg1999g@users.noreply.github.com>
2020-08-23 00:02:14 +0300

    blade template organization.

---
+++ resources/views/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="{{ config('app.locale') }}">
+<head>
+    @include('web.layouts.head')
+</head>
+<body>
+<div id="app">
+    <header>
+        @include('web.layouts.header')
+    </header>
+    <main>
+        @yield('content')
+    </main>
+    <footer>
+        @include('web.layouts.footer')
+    </footer>
+</div>
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}" defer></script>
+</body>
+</html>
