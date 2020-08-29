36919ac489af3fdb5260eb4239957a15a3ddab0f
serg1999g <46674079+serg1999g@users.noreply.github.com>
2020-08-30 02:12:39 +0300

    Implicitly grant admin role all permission checks using can()

--- app/Core/Providers/AuthServiceProvider.php
+++ app/Core/Providers/AuthServiceProvider.php
@@ -13,7 +13,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        // 'App\Model' => 'App\Policies\ModelPolicy',
+//         'App\Model' => 'App\Policies\ModelPolicy',
     ];
 
     /**
@@ -25,6 +25,11 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        //
+        // Implicitly grant "admin" role all permission checks using can()
+        Gate::before(function ($user) {
+            if ($user->hasRole('admin')) {
+                return true;
+            }
+        });
     }
 }
